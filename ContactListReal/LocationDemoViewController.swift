8b8c5adf6784e11efb70d2a2c732d33060312f71
cmbde <crodriguez39@student.gsu.edu>
2025-04-22 22:42:25 -0400

    objs linked + geocoder defined

---
+++ ContactListReal/LocationDemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationDemoViewController.swift
+//  ContactListReal
+//
+//  Created by user272224 on 4/22/25.
+//
+
+import UIKit
+
+class LocationDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
