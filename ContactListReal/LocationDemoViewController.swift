de5c6faddcfbbef2ba3c9b80ad3f2e7144b606dc
cmbde <crodriguez39@student.gsu.edu>
2025-04-22 22:43:20 -0400

    random uncommited change?

--- ContactListReal/LocationDemoViewController.swift
+++ ContactListReal/LocationDemoViewController.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationDemoViewController: UIViewController {
 
+    @IBOutlet weak var txtStreet: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+
+    @IBOutlet weak var lblAltitudeAccuracy: UILabel!
+    @IBOutlet weak var lblAltitude: UILabel!
+    @IBOutlet weak var lblHeading: UILabel!
+    @IBOutlet weak var lblHeadingAccuracy: UILabel!
+    @IBOutlet weak var lblLatitude: UILabel!
+    @IBOutlet weak var lblLongitude: UILabel!
+    @IBOutlet weak var lblLocationAccuracy: UILabel!
+    lazy var geoCoder = CLGeocoder()
+    
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func deviceCoordinates(_ sender: Any) {
+    
+    }
+    @IBAction func addressToCoordinates(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
 
         // Do any additional setup after loading the view.
     }
